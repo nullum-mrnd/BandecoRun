82b2c58566a13e9523c54b5d30e5fa8701d06840
João Victor Miranda <joaovictormiranda.99@gmail.com>
2022-05-23 22:17:46 -0300

    Update BandecoRun.py

--- BandecoRun.py
+++ BandecoRun.py
@@ -10,38 +10,33 @@ menu = Window(1000, 600)
 menu.set_title("BandecoRun")
 fundo = GameImage("Background_1.png")
 click = Window.get_mouse()
+teclado = Window.get_keyboard()
 
 #Limites calçada:
 #cima:
-#esquerda: +/- menu.width/2
+#esquerda: menu.width/2
 #direita: 740
 #baixo:
 
 
-#PLAYER
+## PLAYER
 jogador = Sprite("jogador.png", 1)
 jogador.x = menu.width/2
 jogador.y = menu.height/1.1 - jogador.height/2
 
 
-#ENTITIES SPAWN
-
-
+## OBSTÁCULOS
 
 estudante_vet = [Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5)]
 
+veiculos_vet = [Sprite("veiculos.png",5),Sprite("veiculos.png",5),Sprite("veiculos.png",5),Sprite("veiculos.png",5),Sprite("veiculos.png",5)]
 
-carro1 = Sprite("carro_1.png", 2)
-carro1.x = menu.width/10 - carro1.width/2
-carro1.y = menu.height/4 - carro1.height/2
-
-onibus = Sprite("onibus.png", 1)
-onibus.x = menu.width/2.7 - onibus.width/2
-onibus.y = menu.height/2 - onibus.height/2
 
 moeda1 = Sprite("moeda.png", 1)
 moeda1.x = menu.width/1.7
 moeda1.y = menu.height/4 - moeda1.height/2
+
+## INTERFACE
 
 relogio = Sprite("relógio.png", 1)
 relogio.x = menu.width/2 - relogio.width/2
@@ -83,33 +78,87 @@ pontos = Sprite("pontos.png", 1)
 pontos.x = menu.width/1.15 - pontos.width/2
 pontos.y = menu.height/20 - pontos.height/2
 
-vEntitie = 100
 
-counter=1000
+vEntitie = 100  #velocidade das entidades
+
+##Posições iniciais dos primeiros objetos gerados
 ini = menu.height-332
+ini_veic = menu.height-400
 
-for i in estudante_vet:
-    i.set_curr_frame(randint(0,4))
-    i.x = randint(menu.width/2, 740)
-    i.y = ini
+## Gerador de estudantes:
+for estudante in estudante_vet:
+    estudante.set_curr_frame(randint(0,4))
+    estudante.x = randint(menu.width/2, 740)
+    estudante.y = ini
     ini -= 168
-    
+
+## Gerador de veiculos:
+#for veiculo in veiculos_vet:
+#    veiculo.set_curr_frame(randint(1,4))
+#    veiculo.x = randint(0,menu.width/2 - veiculo.width)
+#    veiculo.y = ini_veic
+#    ini_veic -= 400
+
+## Gerador de veiculos em exatamente duas faixas:
+
+for veiculo in veiculos_vet:
+    veiculo.set_curr_frame(randint(1,4))
+    i = 1
+    aux = i
+    if aux == 1:
+        veiculo.x = -10
+    if aux == 2:
+        veiculo.x = veiculo.width +40
+    veiculo.y = ini_veic
+    ini_veic -= 200
+    i=i+1
     
 
 while True:
 
-    
+## Movimento do player
+    if teclado.key_pressed("LEFT") and jogador.x > 0:
+        jogador.x -= vEntitie * menu.delta_time()
+    if teclado.key_pressed("RIGHT") and jogador.x < 740:
+        jogador.x += vEntitie * menu.delta_time()
+    if teclado.key_pressed("UP") and jogador.y > 0:
+        jogador.y -= vEntitie * menu.delta_time()
+    if teclado.key_pressed("DOWN") and jogador.y < 600-jogador.height:
+        jogador.y += vEntitie * menu.delta_time()
 
+
+## Desenho
     fundo.draw()
     jogador.draw()
 
-    for i in estudante_vet:
-        if i.y > menu.height:
-            i.x = randint(menu.width/2, 740)
-            i.y = -168
-        i.draw()
+    for estudante in estudante_vet:
+        if estudante.y > menu.height:
+            estudante.x = randint(menu.width/2, 740)
+            estudante.y = -168
+        estudante.draw()
 
-    onibus.draw()
+    #for veiculo in veiculos_vet:
+    #    if veiculo.y > menu.height:
+    #        veiculo.x = randint(0,menu.width/2 - veiculo.width)
+    #        veiculo.y = -800
+    #    veiculo.draw()
+
+
+    for veiculo in veiculos_vet:
+        i = 1
+        if veiculo.y > menu.height:
+            aux = i
+            if aux == 1:
+                veiculo.x = -10
+            if aux == 2:
+                veiculo.x = veiculo.width +40
+            if aux == 3:
+                veiculo.x = veiculo.width +80
+            veiculo.y = -800
+        i=i+1
+        veiculo.draw()
+
+
     relogio.draw()
     resist1.draw()
     resist2.draw()
@@ -124,5 +173,8 @@ while True:
 
     for estudante in estudante_vet:
         estudante.y += vEntitie * menu.delta_time()
+
+    #for veiculo in veiculos_vet:
+    #    veiculo.y += vEntitie * menu.delta_time()
 
     menu.update()
