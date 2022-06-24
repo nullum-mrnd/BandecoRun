978ea1a74500c7485fbc377feda0af8a4b2e70eb
Kints1 <igorquintes123@gmail.com>
2022-06-24 16:36:34 -0300

    v.1.4

--- BandecoRun.py
+++ BandecoRun.py
@@ -8,25 +8,33 @@ from interface import *
 
 in_game = Window(1000, 600)
 in_game.set_title("BandecoRun")
-fundo_vet = [Sprite("Background_1.png",1),Sprite("Background_1.png",1),Sprite("Background_1.png",1)]
+fundo_vet = [Sprite("Sprites/GAME/fundo1_sem_predios.png",1),Sprite("Sprites/GAME/fundo1_sem_predios.png",1),Sprite("Sprites/GAME/fundo1_sem_predios.png",1)]
+fundos = [Sprite("Sprites/fundo_menus.png",1), Sprite("Sprites/MENU/fade_bordas.png",1) ]
 click = Window.get_mouse()
 teclado = Window.get_keyboard()
 
+game_status = 0
+tempo_recarga = 0
 
 ## PLAYER
-jogador = Sprite("jogador.png", 1)
+jogador = Sprite("Sprites/GAME/jogador.png", 1)
 jogador.x = in_game.width/2
 jogador.y = in_game.height/1.1 - jogador.height/2
 
 ## OBJETOS:
 
-estudante_vet = [Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5),Sprite("estudantes.png",5)]
-veiculos_vet = [Sprite("veiculos.png",5),Sprite("veiculos.png",5),Sprite("veiculos.png",5),Sprite("veiculos.png",5),Sprite("veiculos.png",5)]
-moedas = [Sprite("moeda.png", 1)]
+estudante_vet = [Sprite("Sprites/GAME/estudantes.png",5),Sprite("Sprites/GAME/estudantes.png",5),Sprite("Sprites/GAME/estudantes.png",5),Sprite("Sprites/GAME/estudantes.png",5),Sprite("Sprites/GAME/estudantes.png",5),Sprite("Sprites/GAME/estudantes.png",5),Sprite("Sprites/GAME/estudantes.png",5),Sprite("Sprites/GAME/estudantes.png",5)]
+veiculos_vet = [Sprite("Sprites/GAME/veiculos.png",5),Sprite("Sprites/GAME/veiculos.png",5),Sprite("Sprites/GAME/veiculos.png",5),Sprite("Sprites/GAME/veiculos.png",5),Sprite("Sprites/GAME/veiculos.png",5)]
+moedas_vet = [Sprite("Sprites/moeda.png", 1), Sprite("Sprites/moeda.png", 1)]
 
 ## INTERFACE
 
-Interface = InterfaceSet(in_game)
+
+Interface_jogo = InterfaceSet(in_game)
+Interface_saida = InterfaceSair(in_game)
+Interface_menu = InterfaceMenu(in_game)
+Interface_loja = InterfaceLoja(in_game)
+cont1 = 0; cont2 = 0; cont3 = 0; cont4 = 0; cont5 = 0
 
 vEntitie = 250  #velocidade das entidades
 
@@ -48,44 +56,105 @@ fundo_vet[2].y = -2 * fundo_vet[1].height
 
 while True:
 
-## Movimento do player
-    if teclado.key_pressed("LEFT") and jogador.x > 0:
-        jogador.x -= vEntitie * 2 * in_game.delta_time()
-    if teclado.key_pressed("RIGHT") and jogador.x < 740:
-        jogador.x += vEntitie * 2 * in_game.delta_time()
 
-###### DRAW #######
+    # ----------- TELA MENU -----------
+    if game_status == 0:
+        InterfaceDraw(fundos)
+        InterfaceDraw(Interface_menu)
 
-## CENARIO
+        #APLICA "CLICK" NOS BOTÕES
+        if (click.is_over_object(Interface_menu[0]) == True ) and (click.is_button_pressed(1)):
+            game_status = 1
+        elif (click.is_over_object(Interface_menu[1]) == True ) and (click.is_button_pressed(1)):
+            game_status = 2
+        elif (click.is_over_object(Interface_menu[2]) == True ) and (click.is_button_pressed(1)):
+            game_status = 0
 
-    for fundo in fundo_vet:
-        fundo.y += vEntitie * in_game.delta_time()
-        if fundo.y > 600:
-            fundo.y = -2 * fundo.height
-        fundo.draw()
+    
+    # ----------- TELA JOGO -----------
 
-## ENTIDADES
+    elif game_status == 1:
+        ## Movimento do player
+        if teclado.key_pressed("LEFT") and jogador.x > 0:
+                jogador.x -= vEntitie * 2 * in_game.delta_time()
+        if teclado.key_pressed("RIGHT") and jogador.x < 740:
+                jogador.x += vEntitie * 2 * in_game.delta_time()
 
-    jogador.draw()
+        ###### DRAW #######
 
-    EntitieDrawer(estudante_vet, randint(0,5), in_game.height, -200, randint(in_game.width/2, 740))
-    EntitieDrawer(moedas, 1, in_game.height, -randint(100,500),randint(0, 740))
-    VehicleDrawer(veiculos_vet, randint(1,4), in_game.height, -800)
+        ## CENARIO
+
+        for fundo in fundo_vet:
+            fundo.y += vEntitie * in_game.delta_time()
+            if fundo.y > 600:
+                fundo.y = -2 * fundo.height
+            fundo.draw()
+
+        ## ENTIDADES
+
+        jogador.draw()
+
+        EntitieDrawer(estudante_vet, randint(0,5), in_game.height, -200, randint(in_game.width/2, 740))
+        EntitieDrawer(moedas_vet, randint(0,5), in_game.height, -randint(100,500),randint(0, 740))
+        VehicleDrawer(veiculos_vet, randint(1,4), in_game.height, -800)
 
 
-## MOVIMENTAÇÃO DOS OBJETOS
+        ## MOVIMENTAÇÃO DOS OBJETOS
 
-    for estudante in estudante_vet:
-        estudante.y += vEntitie * in_game.delta_time()
+        for estudante in estudante_vet:
+            estudante.y += vEntitie * in_game.delta_time()
 
-    for veiculo in veiculos_vet:
-        veiculo.y += vEntitie* 2 * in_game.delta_time()
+        for veiculo in veiculos_vet:
+            veiculo.y += vEntitie* 2 * in_game.delta_time()
 
-    for moeda in moedas:
-        moeda.y += vEntitie * in_game.delta_time()
+        for moeda in moedas_vet:
+            moeda.y += vEntitie * in_game.delta_time()
 
-## INTERFACE
+        ## INTERFACE
+        InterfaceDraw(Interface_jogo)
 
-    InterfaceDraw(Interface)
+        #CONTROLE DE SAIDA
+        if (teclado.key_pressed("ESC") == 1): 
+            InterfaceDraw(Interface_saida)
+            if(click.is_over_object(Interface_saida[1]) == True ) and (click.is_button_pressed(1)):
+                game_status = 0
+                    
+
+    elif game_status == 2:
+        Interface_num_loja = InterfaceNumLoja(in_game, 0 + cont1, 0 + cont2, 0 + cont3, 0 + cont4, 0 + cont5)
+        InterfaceDraw(fundos)
+        InterfaceDraw(Interface_loja)
+        InterfaceDraw(Interface_num_loja)
+
+        #VOLTA AO MENU
+        if (teclado.key_pressed("ESC") == 1):      
+            game_status = 0
+        if (click.is_over_object(Interface_loja[0]) == True ) and (click.is_button_pressed(1)):
+            game_status = 0
+
+        #MUDA O VALOR DA QUANTIDADE DE ITENS COMPRADOS
+    
+        tempo_recarga += in_game.delta_time()
+
+        if (click.is_over_object(Interface_loja[1]) == True ) and (click.is_button_pressed(1)) and (tempo_recarga > 0.5) and (cont1 <= 4) :
+            cont1 += 1
+            tempo_recarga = 0
+
+        elif (click.is_over_object(Interface_loja[2]) == True ) and (click.is_button_pressed(1)) and (tempo_recarga > 0.5) and (cont2 <= 4) :
+            cont2 += 1
+            tempo_recarga = 0
+
+        elif (click.is_over_object(Interface_loja[3]) == True ) and (click.is_button_pressed(1)) and (tempo_recarga > 0.5) and (cont3 <= 4) :
+            cont3 += 1
+            tempo_recarga = 0
+
+        elif (click.is_over_object(Interface_loja[4]) == True ) and (click.is_button_pressed(1)) and (tempo_recarga > 0.5) and (cont4 <= 4) :
+            cont4 += 1
+            tempo_recarga = 0
+
+        elif (click.is_over_object(Interface_loja[5]) == True ) and (click.is_button_pressed(1)) and (tempo_recarga > 0.5) and (cont5 <= 4) :
+            cont5 += 1
+            tempo_recarga = 0
+
 
     in_game.update()
--- interface.py
+++ interface.py
@@ -9,14 +9,73 @@ def SetObjeto(sprite, pos_x, pos_y):
     return objeto
 
 def InterfaceSet(window):
-    relogio = SetObjeto(["relógio.png",1], window.width/2, window.height/20)
-    resist = SetObjeto(["ponto_de_resistencia.png", 1], window.width/40, window.height/20)
-    power = SetObjeto(["power.png", 1], window.width/30, window.height/1.1)
-    qnt_moeda = SetObjeto(["quantidade_moeda.png", 1], window.width/1.04, window.height/1.1)
-    moeda_icon = SetObjeto(["moeda.png", 1], window.width/1.1, window.height/1.1)
-    pontos = SetObjeto(["pontos.png", 1], window.width/1.15, window.height/20)
+    relogio = SetObjeto(["Sprites/GAME/relógio.png",1], window.width/2, window.height/20)
+    resist = SetObjeto(["Sprites/GAME/ponto_de_resistencia.png", 1], window.width/40, window.height/20)
+    power = SetObjeto(["Sprites/GAME/power.png", 1], window.width/30, window.height/1.1)
+    qnt_moeda = SetObjeto(["Sprites/GAME/quantidade_moeda.png", 1], window.width/1.04, window.height/1.1)
+    moeda_icon = SetObjeto(["Sprites/GAME/moeda.png", 1], window.width/1.1, window.height/1.1)
+    pontos = SetObjeto(["Sprites/GAME/pontos.png", 1], window.width/1.15, window.height/20)
+    
 
     Interface = [relogio, resist, power, qnt_moeda, moeda_icon, pontos]
+
+    return Interface
+
+def InterfaceSair(window):
+    sair = SetObjeto(["Saida.png",1], window.width/2, window.height/2)
+    sim = SetObjeto(["Saida(sim).png",1], window.width/2.5, window.height/1.8)
+    nao = SetObjeto(["Saida(nao).png",1], window.width/1.7, window.height/1.8)
+
+    Interface = [sair, sim, nao]
+    return Interface
+
+def InterfaceMenu(window):
+    jogar = SetObjeto(["Sprites/MENU/jogar.png",1], window.width/2, window.height/1.7)
+    loja = SetObjeto(["Sprites/MENU/loja.png",1], window.width/2, window.height/1.5 + 15)
+    creditos = SetObjeto(["Sprites/MENU/creditos.png",1], window.width/2, window.height/1.05)
+    controladores = SetObjeto(["Sprites/MENU/controles.png",1], window.width/10, window.height/1.4)
+    tecla_a = SetObjeto(["Sprites/MENU/tecla_a.png",1], window.width/10 - 30, window.height/1.3 + 5)
+    tecla_d = SetObjeto(["Sprites/MENU/tecla_d.png",1], window.width/10 + 30, window.height/1.3 + 5)
+    power_ups = SetObjeto(["Sprites/MENU/power_ups.png",1], window.width/10, window.height/1.15)
+    tecla_1 = SetObjeto(["Sprites/MENU/tecla_1.png",1], window.width/10 - 30, window.height/1.1 + 10)
+    tecla_2 = SetObjeto(["Sprites/MENU/tecla_2.png",1], window.width/10 + 30, window.height/1.1 + 10)
+    bandeco_logo = SetObjeto(["Sprites/MENU/logo_bandeco.png",1], window.width/2, window.height/4)
+
+    Interface = [jogar, loja, creditos, controladores, tecla_a, tecla_d, power_ups, tecla_1, tecla_2, bandeco_logo]
+
+    return Interface
+
+def InterfaceLoja(window):
+    voltar = SetObjeto(["Sprites/LOJA/voltar.png",1], window.width/2, window.height/1.05)
+    janela1 = SetObjeto(["Sprites/LOJA/icones.png", 1], 120, 310)
+    janela2 = SetObjeto(["Sprites/LOJA/icones.png", 1], 220, 310)
+    janela3 = SetObjeto(["Sprites/LOJA/icones.png", 1], 450, 310)
+    janela4 = SetObjeto(["Sprites/LOJA/icones.png", 1], 550, 310)
+    janela5 = SetObjeto(["Sprites/LOJA/icones.png", 1], 840, 310)
+    loja = SetObjeto(["Sprites/LOJA/loja.png",1], window.width/2, window.height/7)
+    sapatos = SetObjeto(["Sprites/LOJA/sapatos_titulo.png",1], 170, 250)
+    power_ups = SetObjeto(["Sprites/LOJA/power_ups_titulo.png",1], window.width/2, 250)
+    resist = SetObjeto(["Sprites/LOJA/pontos_de_resistencia_titulo.png",1], 840, 240)
+
+
+    Interface = [voltar, janela1, janela2, janela3, janela4, janela5, loja, sapatos, power_ups, resist]
+
+    return Interface
+
+def InterfaceNumLoja(window, frame1, frame2, frame3, frame4, frame5):
+    num_janela1 = SetObjeto(["Sprites/LOJA/numero_pequeno.png", 10], 120, 337)
+    num_janela1.set_curr_frame(frame1)
+    num_janela2 = SetObjeto(["Sprites/LOJA/numero_pequeno.png", 10], 220, 337)
+    num_janela2.set_curr_frame(frame2)
+    num_janela3 = SetObjeto(["Sprites/LOJA/numero_pequeno.png", 10], 450, 337)
+    num_janela3.set_curr_frame(frame3)
+    num_janela4 = SetObjeto(["Sprites/LOJA/numero_pequeno.png", 10], 550, 337)
+    num_janela4.set_curr_frame(frame4)
+    num_janela5 = SetObjeto(["Sprites/LOJA/numero_pequeno.png", 10], 840, 337)
+    num_janela5.set_curr_frame(frame5)
+
+
+    Interface = [num_janela1, num_janela2, num_janela3, num_janela4, num_janela5]
 
     return Interface
 
